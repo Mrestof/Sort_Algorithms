098c1bb318c01266bf608a2b2f6bf439733329e4
Mrestof <naruto.golubev@gmail.com>
2019-08-21 17:08:34 +0300

    Fixed RecursionError bug!!
    Change test function

--- Sorting_Algorithms/Quick_Sort.py
+++ Sorting_Algorithms/Quick_Sort.py
@@ -1,5 +1,3 @@
-from random import randint as rint
-
 file = open('nums.txt', 'r')
 file_list = file.read().split(':')
 file.close()
@@ -13,19 +11,16 @@ print('Have this list: \n' + str(num_list) + '\nStart sorting...')
 
 
 def quick_sort(l: list):
-    """
-    This function crashes with RecursionError if some numbers in list that it takes are the same (need to fix)
-    """
     if len(l) <= 1:
         return l
 
-    bp = rint(0, len(l) - 1)
-    l[bp], l[-1] = l[-1], l[bp]
-    print(f'Swap last and "{bp}" elements\n{l}')
-    bp = -1
+    bp = len(l) - 1
+    # l[bp], l[-1] = l[-1], l[bp]
+    # print(f'Swap last and "{bp}" elements\n{l}')
+    # bp = -1
 
     lp = 0
-    rp = len(l) - 1 if bp != len(l) - 1 else bp - 1
+    rp = bp - 1
 
     print('b_point: ' + str(bp) + '\nl_point: ' + str(lp) + '\nr_point: ' + str(rp))
 
@@ -36,23 +31,26 @@ def quick_sort(l: list):
         while l[rp] >= l[bp] and rp > lp:
             rp -= 1
 
-        if rp == lp:
+        if rp == lp or lp == bp:
             sorted_pointer = lp
             print('s_point ' + str(sorted_pointer))
-            l[bp], l[rp] = l[rp], l[bp]
+            l[bp], l[lp] = l[lp], l[bp]
             break
 
-        print('changing r_point and l_point')
+        # print('changing r_point and l_point')
         l[rp], l[lp] = l[lp], l[rp]
 
     l1 = l[:sorted_pointer]
-    l2 = l[sorted_pointer:]
+    l2 = l[sorted_pointer + 1:]
 
-    return quick_sort(l1) + quick_sort(l2)
+    tmp_list = quick_sort(l1)
+    tmp_list.append(l[sorted_pointer])
+
+    return tmp_list + quick_sort(l2)
 
 
 def sort_test(original_list: list, sorted_list: list):
-    print('=' * len(str(num_list)))
+    print('===' * 30 + "\nTesting...\n" + '===' * 30)
     list1 = list(original_list)
     list1.sort()
     if list1 == sorted_list:
